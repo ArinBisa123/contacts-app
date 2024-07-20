9d6050d461f1b076c7ec0885fda9e7e15165bbc1
ArinBisa123 <leejindairy06@gmail.com>
2024-07-20 20:51:13 +0700

    fix error, set query and path params

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,12 +2,25 @@ import ContactList from "../components/ContactList";
 import { deleteContact, getContacts } from "../utils/data";
 import React from "react";
 import SearchBar from "../components/SearchBar";
+import { useSearchParams } from "react-router-dom";
+
+function HomepageWrapper() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const keyword = searchParams.get("keyword");
+  function changeSearchParams(keyword) {
+    setSearchParams({ keyword });
+  }
+  return (
+    <Homepage defaultKeyword={keyword} keywordChange={changeSearchParams} />
+  );
+}
 
 class Homepage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       contacts: getContacts(),
+      keyword: props.defaultKeyword || "",
     };
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
     this.keywordChangeHandler = this.keywordChangeHandler.bind(this);
@@ -18,7 +31,6 @@ class Homepage extends React.Component {
     this.setState(() => {
       return {
         contacts: getContacts(),
-        keyword: "",
       };
     });
   }
@@ -26,6 +38,7 @@ class Homepage extends React.Component {
     this.setState(() => {
       return { keyword };
     });
+    this.props.keywordChange(keyword);
   }
   render() {
     const contacts = this.state.contacts.filter((contact) => {
@@ -46,4 +59,4 @@ class Homepage extends React.Component {
   }
 }
 
-export default Homepage;
+export default HomepageWrapper;
